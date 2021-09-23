b403b0397110b77779f7220620a80d6f872f1e24
anjirajardula <anjirajardula@gmail.com>
2021-09-23 22:42:53 +0530

    Modified:modified the code and change the naming convesion

--- src/com/bridgelabz/StopWatch.java
+++ src/com/bridgelabz/StopWatch.java
@@ -2,12 +2,18 @@ package com.bridgelabz;
 
 import java.util.Scanner;
 
+/**
+ * @author : Anjiraj
+ * @version : 16.0
+ * @since : 23/09/2021
+ */
+
 public class StopWatch {
-    /*
-     * Ask User To Press A Key To Start The Time
-     * Ask User To Press A Key To Stop The Time
-     * Calculate The Elapsed Time
-     */
+
+    //  Ask User To Press A Key To Start The Time
+    //Ask User To Press A Key To Stop The Time
+    // Calculate The Elapsed Time
+
     public static void main(String[] args) {
         //Welcome Message Added
         System.out.println("Welcome To The Stop Watch Program");
@@ -21,7 +27,7 @@ public class StopWatch {
         char stopch = sc.next().charAt(0);
         Long stop = System.currentTimeMillis();
         //Condition To Calculate The Elapsed Time
-        float timeElapsed = (float) (stop - start)/1000;
-        System.out.println("Time = "+ timeElapsed + " Seconds");
+        float timeElapsed = (float) (stop - start) / 1000;
+        System.out.println("Time = " + timeElapsed + " Seconds");
     }
 }
