5df3767c0a0658c51ccfdedfe6316114325793e0
anjirajardula <anjirajardula@gmail.com>
2021-09-23 22:10:23 +0530

    Modified:checkuing and refactor the code

--- src/com/bridgelabz/CoupanNumber.java
+++ src/com/bridgelabz/CoupanNumber.java
@@ -1,17 +1,23 @@
 package com.bridgelabz;
 
+/**
+ * @author :Anjiraj
+ * @version : 16.0
+ * @since : 23/09/2021
+ */
+
 public class CoupanNumber {
-    /*
-     *assigning the characters for coupon
-     * giving the max. size of coupon
-     **/
+
+     //assigning the characters for coupon
+     //giving the max. size of coupon
+
 
     public static void main(String[] args) {
         char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789".toCharArray();
         int max = 100000000;
         int random = (int) (Math.random() * max); //random function to generate coupon code
         StringBuffer sb = new StringBuffer();
-
+        // while loop
         while (random > 0) {
             sb.append(chars[random % chars.length]);
             random /= chars.length;
